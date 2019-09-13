127ee4c9c7ce1cabcccaba480c832f11d03dad58
Bruce Marriner <bruce@sqls.net>
2019-09-13 08:33:38 -0500

    Added JSONObject, handle DECIAML for JSON better

--- json.go
+++ json.go
@@ -6,33 +6,73 @@ import (
 	"io"
 )
 
-func (s *SQLto) JSON(w io.Writer) error {
+// JSONArray converts sql rows into an array of JSON arrays
+func (s *SQLto) JSONArray(w io.Writer) error {
 	var err error
 
-	colNames, err := s.Rows.Columns()
+	cols, err := s.Rows.ColumnTypes()
 	if err != nil {
-		return fmt.Errorf("error fetching column names, %s\n", err)
+		return fmt.Errorf("error fetching column types, %s\n", err)
 	}
-	length := len(colNames)
+	colsLen := len(cols)
 
-	pointers := make([]interface{}, length)
-	container := make([]interface{}, length)
-	for i := range pointers {
-		pointers[i] = &container[i]
+	values := make([]interface{}, colsLen)
+	for i := range values {
+		values[i] = &GenericScanner{dbtype: cols[i].DatabaseTypeName()}
 	}
 
-	jenc := json.NewEncoder(w)
+	j := json.NewEncoder(w)
 	w.Write([]byte("["))
 	delim := []byte("")
 
 	for s.Rows.Next() {
-		err = s.Rows.Scan(pointers...)
+		err = s.Rows.Scan(values...)
 		if err != nil {
 			return fmt.Errorf("error scanning sql row, %s\n", err)
 		}
 
 		w.Write(delim)
-		err = jenc.Encode(container)
+		err = j.Encode(values)
+		delim = []byte(",")
+		if err != nil {
+			return fmt.Errorf("error encoding data, %s", err)
+		}
+
+	}
+	w.Write([]byte("]"))
+	return nil
+}
+
+// JSONObject converts sql rows into an array of JSON objects
+func (s *SQLto) JSONObject(w io.Writer) error {
+	var err error
+
+	cols, err := s.Rows.ColumnTypes()
+	if err != nil {
+		return fmt.Errorf("error fetching column information, %s\n", err)
+	}
+	colsLen := len(cols)
+
+	// scan is needed here because Rows.Scan requires a []interface{} argument
+	scan := make([]interface{}, colsLen)
+	values := make(map[string]interface{})
+	for i, v := range cols {
+		values[v.Name()] = &GenericScanner{dbtype: v.DatabaseTypeName()}
+		scan[i] = values[v.Name()]
+	}
+
+	j := json.NewEncoder(w)
+	w.Write([]byte("["))
+	delim := []byte("")
+
+	for s.Rows.Next() {
+		err = s.Rows.Scan(scan...)
+		if err != nil {
+			return fmt.Errorf("error scanning sql row, %s\n", err)
+		}
+
+		w.Write(delim)
+		err = j.Encode(values)
 		delim = []byte(",")
 		if err != nil {
 			return fmt.Errorf("error encoding data, %s", err)
---
+++ scanner.go
@@ -0,0 +1,44 @@
+package sqlto
+
+import (
+	"encoding/json"
+	"strconv"
+)
+
+// GenericScanner is a somewhat hacky way of dealing with SQL database
+// types that are not well represnted in Go.  For example, a DECIAML type
+// has no matching Go datatype and if we do nothing, then it will be JSON
+// Marshaled into a base64 string which isn't what we want for this library
+// use case.
+// This is all a big of "magic box" stuff here that isn't very idomatic Go
+// so if someone knows a better solution to this, I'd love to hear it.
+type GenericScanner struct {
+	value  interface{}
+	dbtype string
+	want   string
+}
+
+func (scanner *GenericScanner) MarshalJSON() ([]byte, error) {
+	return json.Marshal(scanner.value)
+}
+
+func (scanner *GenericScanner) Scan(src interface{}) error {
+	switch src.(type) {
+	case []byte:
+		switch scanner.dbtype {
+		case "DECIMAL":
+			f, err := strconv.ParseFloat(string(src.([]byte)), 64)
+			if err != nil {
+				scanner.value = string(src.([]byte))
+			} else {
+				scanner.value = f
+			}
+		default:
+			scanner.value = string(src.([]byte))
+		}
+	default:
+		scanner.value = src
+
+	}
+	return nil
+}
